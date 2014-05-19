1a4ab1ecfc6868d0b0045b1b4278fefd34e32e58
Riley Shaw <rileyjshaw@gmail.com>
2014-05-19 02:46:44 -0400

    Refactor slightly, add comments

--- main.js
+++ main.js
@@ -1,106 +1,133 @@
-function substringify(lengths, string) {
-  var i = 0;
+var glorious = (function() {
+  var exports = {};
 
-  return lengths.map(function(len) {
-    var substring = string.substr(i, len);
-    i += len;
-    return substring;
-  });
-}
+  // takes a character pattern and a list of words
+  // and returns true if they follow the same pattern
+  exports.match = function(pattern, data) {
+    var patternLength = pattern.length;
+    var matches = {}, matchValues = {};
+    var curPattern, curData;
 
-function tryPossibleConfigurations(coefficients, patternLength, dataLength, func) {
-  function weightedAdd(accum, cur, i) {
-    return accum + cur * lengths[i];
-  }
-  var lengths = [];
-  var totalDepth = coefficients.length;
+    // handles arrays and strings
+    if (typeof pattern === 'string') {
+      pattern = pattern.split('');
+    }
 
-  return (function level(depth) {
-    var fullLength;
-    var innermost = (depth === totalDepth - 1);
-    var curCharCount = coefficients[depth];
-    var _maxLen = (dataLength - patternLength + curCharCount) / curCharCount;
-    for(var i = 1; i <= _maxLen; i++) {
-      lengths[depth] = i;
+    // handles arrays and space-delimited strings
+    if (typeof data === 'string') {
+      data = data.split(' ');
+    }
 
-      if (!innermost) {
-        if (level(depth + 1)) {
-          return true;
+    if (patternLength !== data.length) {
+      return false;
+    }
+
+    for (var i = 0; i < patternLength; i++) {
+      curPattern = pattern[i];
+      curData = data[i];
+
+      if (!matches[curPattern]) {
+        if (!matchValues[curData]) {
+          matches[curPattern] = curData;
+          matchValues[curData] = true;
+        } else return false;
+      } else { // already recorded
+        if (matches[curPattern] !== curData) {
+          return false;
         }
-      } else {
-        fullLength = coefficients.reduce(weightedAdd, 0);
+      }
+    }
+    return true;
+  }
 
-        if (fullLength < dataLength) continue;
-        else if (fullLength === dataLength) {
-          if (func(lengths)) {
-            return true; // break out of all loops (bubbles up)
+
+  // takes a string and returns an array of substrings.
+  // splits are determined by the 'lengths' array
+  function substringify(lengths, string) {
+    var i = 0;
+
+    return lengths.map(function(len) {
+      var substring = string.substr(i, len);
+      i += len;
+      return substring;
+    });
+  }
+
+
+  // finds likely substrings from non-delimited data
+  // and runs them through the original match()
+  function tryPossibleConfigurations(coefficients, patternLength, dataLength, func) {
+    function weightedAdd(accum, cur, i) {
+      return accum + cur * lengths[i];
+    }
+    // lengths holds the substring lengths represented by each character
+    var lengths = [];
+    var totalDepth = coefficients.length;
+
+    // each level of the recursive call represents a unique
+    // character in the pattern (in the order that they first appear)
+    function loopThroughChars(depth) {
+      var isInnermost = (depth === totalDepth - 1);
+      var curCharCount = coefficients[depth];
+      var derivedDataLength;
+      // maxCharLength is the max substring length that this character can represent
+      var maxCharLength = (dataLength - patternLength + curCharCount) / curCharCount;
+
+      for(var i = 1; i <= maxCharLength; i++) {
+        lengths[depth] = i;
+
+        if (!isInnermost) {
+          if (loopThroughChars(depth + 1)) {
+            return true; // allows us to bubble up a true return
+          }
+        } else {
+          derivedDataLength = coefficients.reduce(weightedAdd, 0);
+
+          if (derivedDataLength < dataLength) continue;
+          else if (derivedDataLength === dataLength) {
+            if (func(lengths)) {
+              return true; // break out of all loops (bubbles up)
+            }
+          }
+          else {
+            return false; // breaks out of this loop (no bubbling)
           }
         }
-        else {
-          return false; // breaks out of this loop (no bubbling)
-        }
       }
-    }
-    return false;
-  })(0);
-}
 
-function match(pattern, data) {
-  var patternLength = pattern.length;
-  var matches = {}, matchValues = {};
-  var curPattern, curData;
+      return false;
+    };
 
-  if (typeof pattern === 'string') {
-    pattern = pattern.split('');
+    return loopThroughChars(0);
   }
 
-  if (typeof data === 'string') {
-    data = data.split(' ');
-  }
 
-  if (patternLength !== data.length) {
-    return false;
-  }
+  exports.megamatch = function(pattern, data) {
+    var splitPattern = pattern.split('');
+    var patternLength = splitPattern.length;
+    var dataLength = data.length;
+    // coefficients stores the count of each character
+    // charIdx maps a character to its index in coefficients
+    var coefficients = [], charIdx = {};
 
-  for (var i = 0; i < patternLength; i++) {
-    curPattern = pattern[i];
-    curData = data[i];
-
-    if (!matches[curPattern]) {
-      if (!matchValues[curData]) {
-        matches[curPattern] = curData;
-        matchValues[curData] = true;
-      } else return false;
-    } else { // already recorded
-      if (matches[curPattern] !== curData) {
-        return false;
-      }
-    }
-  }
-  return true; // yay
-}
-
-function match2(pattern, data) {
-  var splitPattern = pattern.split('');
-  var patternLength = splitPattern.length;
-  var dataLength = data.length;
-  // coefficients stores the count of each character
-  // charIdx maps a character to its index in coefficients
-  var coefficients = [], charIdx = {};
-
-  // populate coefficients and charIdx
-  splitPattern.forEach(function(char) {
-    if (charIdx[char] === undefined) {
-      charIdx[char] = coefficients.push(1) - 1;
-    } else ++coefficients[ charIdx[char] ];
-  });
-
-  // find possible substring length groups
-  return tryPossibleConfigurations(coefficients, patternLength, dataLength, function(lengths) {
-    var substrings = splitPattern.map(function(char) {
-      return lengths[ charIdx[char] ];
+    // populate coefficients and charIdx
+    splitPattern.forEach(function(char) {
+      if (charIdx[char] === undefined) {
+        charIdx[char] = coefficients.push(1) - 1;
+      } else ++coefficients[ charIdx[char] ];
     });
-    var splitData = substringify(substrings, data);
-    return match(splitPattern, splitData);
-  });
-}
+
+    function splitAndMatch(lengths) {
+      var substrings = splitPattern.map(function(char) {
+        return lengths[ charIdx[char] ];
+      });
+      var splitData = substringify(substrings, data);
+      return exports.match(splitPattern, splitData);
+    }
+
+    return tryPossibleConfigurations(coefficients, patternLength, dataLength, splitAndMatch);
+  }
+
+
+  return exports;
+})();
